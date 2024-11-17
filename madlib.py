9c7285aadfca326cda842132c17529cefdf52a1a
İlahə <132819071+ilahebayramzade@users.noreply.github.com>
2024-11-17 11:22:30 +0400

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,22 @@
+print("About me \n\nLet's get started\n")
+your_name = input("Your Name: ")
+number1 = int(input("Number: "))
+noun1 = input("Noun: ")
+adjective1 = input("Adjective: ")
+where = input("Where: ")
+person = input("Person: ")
+color = input("Color: ")
+animal = input("Animal: ")
+noun2 = input("Noun: ")
+number2 = int(input("Number: "))
+verb = input("Verb: ")
+place = input("Country, city wherever place: ")
+adjective2 = input("Adjective: ")
+noun3 = input("Noun: ")
+
+print(f"""      My name is {your_name}. I am {number1} years old. I want to admit that I have {noun1} and I love it. 
+      By the way, I would like to share one of my memories and I think this is really {adjective1}. 
+      One day, I went to the {where} at 2:00 AM and I saw {person} here, and when that person saw me, said that you are my {color} {animal}.
+      I was shocked; then we talked about {noun2} for {number2} hours. Finally, I came back home. 
+      Besides, another fact about me is <I love to {verb}>, but no one knows this because I don't say it to anyone. 
+      To go to {place} is my dream. That is why I will buy {adjective2} {noun3}. That's all.""")
